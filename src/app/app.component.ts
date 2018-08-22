c030e4ffb305b6c50ff0f3f3dd79b8cec1989ec8
krasnof.kobi@gmail.com <krasnoff.kobi@gmail.com>
2018-08-22 11:51:52 +0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {AppService} from './app.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [AppService]
 })
-export class AppComponent {
-  title = 'books-list-application';
+export class AppComponent implements OnInit, OnDestroy {
+  books: Array<any>;
+  subscription: Subscription = null;
+
+  constructor(private _httpService:AppService) {}
+
+  ngOnInit() {
+    this.subscription = this._httpService.getMethod("json/booksList.json")
+    .subscribe (
+      data => {
+        this.books = data;
+      },
+      error => {
+        this.books = [];
+      })
+  };
+
+  ngOnDestroy() { 
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
